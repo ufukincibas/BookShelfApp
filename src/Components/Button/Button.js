b1029eb8f7c71c3d12878c3bff0737b0cff2b3f1
ufukincibas <ufukincibas@hotmail.com>
2024-11-11 00:45:23 +0300

    Button Component Updated.

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { Text , TouchableOpacity} from "react-native";
+import { TouchableOpacity, Text } from "react-native";
+import styles from "./Button.styles"; // Stilleri ayrı dosyadan içe aktarıyoruz.
 
-import styles from "./Button.styles"
-
-function Button(){
-    return(
-        <TouchableOpacity>
-            <Text></Text>
-      </TouchableOpacity>
-    )
-}
+const Button = ({ title, onPress, disabled = false , theme = "Primary"}) => {
+  //default olarak primary verdik  her seferinde primary yazmana gerek kalmaz sadece secondary kullanacağın yerde yazarsın . !!
+  return (
+    <TouchableOpacity 
+      style={[styles[theme].button
+        , disabled ? styles.buttonDisabled : null]} 
+      onPress={onPress} 
+      disabled={disabled} // Buton devre dışıysa basılamaz.
+    >
+      <Text style={styles[theme].buttonText}>{title}</Text> 
+    </TouchableOpacity>
+  );
+};
 
 export default Button;
