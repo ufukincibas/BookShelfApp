39e1dafecb91735db9001590ba71b7fab3eb9ef1
ufukincibas <ufukincibas@hotmail.com>
2024-11-10 23:58:32 +0300

    Drawer Navigation Added

---
+++ android/app/.cxx/Debug/4y4j72l5/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "C:/Users/ufuk/.gradle/caches/8.10.2/transforms/a3e9d651bc510bdf1b43157f48206999/transformed/hermes-android-0.76.1-debug/prefab/modules/libhermes/libs/android.armeabi-v7a/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/ufuk/.gradle/caches/8.10.2/transforms/a3e9d651bc510bdf1b43157f48206999/transformed/hermes-android-0.76.1-debug/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
---
+++ src/Pages/Auth/SignIn/SignIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text , View } from "react-native";
+
+function SignIn(){
+    return(
+        <View>
+            <Text>Hello From SignIn</Text>
+        </View>
+    )
+}
+
+export default SignIn;
